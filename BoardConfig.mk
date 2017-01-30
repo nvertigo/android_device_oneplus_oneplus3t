ce3f548bc4c00c6b70caa8ef10ed8024376eb956
nvertigo <nvertigo67@gmail.com>
2017-01-30 20:45:47 +0100

    BoardConfig.mk: set TARGET_KERNEL_CROSS_COMPILE_PREFIX to use UBERTC/linaro toolchain.
    Change-Id: Ic2bf07ec968898c116833df49e880a8847c244f6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,7 +72,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8996
 TARGET_KERNEL_CONFIG := lineageos_oneplus3t_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := ../../../../../../prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9-kernel/bin/aarch64-linux-android-
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
