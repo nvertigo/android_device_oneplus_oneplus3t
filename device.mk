b10093517261751024af0db2dac4f538abdc14cd
nvertigo <nvertigo67@gmail.com>
2017-02-13 13:31:08 +0100

    device.mk: remove dublicate org.ifaa.android.manager.
    Change-Id: I3d465caa0ef7f41b76b39ee729d6acb76ac72632

--- device.mk
+++ device.mk
@@ -111,9 +111,6 @@ PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_PACKAGES += \
     org.ifaa.android.manager
 
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
-
 # Audio
 PRODUCT_PACKAGES += \
     audiod \
