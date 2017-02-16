c08e8ec5d6dc628755fb884b654e9fdda4c80f2c
dianlujitao <dianlujitao@lineageos.org>
2017-02-16 23:14:21 +0800

    op3: Stop inheriting from shared graphics driver repository
     * Pin these LA.UM.5.5.r1-02200-8x96.0 blobs
    
    Change-Id: I2172a8edffe6c3d86cc27307d9ff2cf6523442a5

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -76,9 +76,9 @@ cat << EOF >> "$ANDROIDMK"
 
 endif
 
+\$(shell mkdir -p \$(PRODUCT_OUT)/system/vendor/lib/egl && pushd \$(PRODUCT_OUT)/system/vendor/lib > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
+\$(shell mkdir -p \$(PRODUCT_OUT)/system/vendor/lib64/egl && pushd \$(PRODUCT_OUT)/system/vendor/lib64 > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
 EOF
-
-printf '\n%s\n' "\$(call inherit-product, vendor/qcom/binaries/msm8996/graphics/graphics-vendor.mk)" >> "$PRODUCTMK"
 
 # Append the calls to firmware images
 append_firmware_calls_to_makefiles
