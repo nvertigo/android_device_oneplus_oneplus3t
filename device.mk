a3e26a65d5815194b89b20d8e3553d5f1ec0c8db
nvertigo <nvertigo67@gmail.com>
2017-02-26 11:10:37 +0100

    Revert "device.mk: remove dublicate org.ifaa.android.manager."
    This reverts commit b10093517261751024af0db2dac4f538abdc14cd.

--- device.mk
+++ device.mk
@@ -111,6 +111,9 @@ PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_PACKAGES += \
     org.ifaa.android.manager
 
+PRODUCT_BOOT_JARS += \
+    org.ifaa.android.manager
+
 # Audio
 PRODUCT_PACKAGES += \
     audiod \
