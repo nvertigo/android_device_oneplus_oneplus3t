c1a98091de3a5051c019cbf4ba84f5aec71b86e3
Nvertigo <nvertigo67@gmail.com>
2018-06-04 09:36:35 +0200

    op3/t: use gcc-7.3 for kernel building.
    Change-Id: I92278d2ce5388614ad2ccd4179b70e7a733dd60b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,7 +70,7 @@ BOARD_RAMDISK_OFFSET     := 0x02200000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8996
 TARGET_KERNEL_CONFIG := lineageos_oneplus3_defconfig
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-7.x/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-7.3/bin
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # QCOM hardware
